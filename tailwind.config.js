48981074262534d52f803335558cb56127ad3d69
Sakshiwable <wablesakshi9@gmail.com>
2024-09-27 09:23:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Add the file paths relevant to your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
